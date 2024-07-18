44d3e2e4c1fdd8a328495f6a9c31bf5950291338
JamieBirch <jamie.d.birch@gmail.com>
2024-07-18 11:02:47 +0200

    Implement read ingredient values from json

--- Assets/Scripts/IngredientValueManager.cs
+++ Assets/Scripts/IngredientValueManager.cs
@@ -1,33 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class IngredientValueManager : MonoBehaviour
 {
-    public static IngredientValueManager instance;
     public static Dictionary<IngredientType, int> ingredientValueDictionary = new();
-    public IngredientValue[] ingredientValues;
+    public static string IngredientValueJsonPath = "Assets/ingredient_value.json";
     
     [Serializable]
     public class IngredientValue
     {
-        public IngredientType type;
+        public string type;
         public int value;
+    }
+    
+    [Serializable]
+    private class IngredientValueArrayWrapper
+    {
+        public IngredientValue[] ingredients;
     }
     
     private void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-        }
-
         if (ingredientValueDictionary.Count == 0)
         {
-            foreach (IngredientValue ingredientValue in ingredientValues)
+            string json = ExtractJsonString();
+
+            if (!string.IsNullOrEmpty(json))
             {
-                ingredientValueDictionary.Add(ingredientValue.type, ingredientValue.value);
+                // Deserialize the JSON data to the IngredientValueArrayWrapper class
+                IngredientValueArrayWrapper ingredientDataWrapper = JsonUtility.FromJson<IngredientValueArrayWrapper>(json);
+
+                // Populate the dictionary with the deserialized data
+                foreach (var ingredientValue in ingredientDataWrapper.ingredients)
+                {
+                    if (Enum.TryParse(ingredientValue.type, true, out IngredientType ingredientType))
+                    {
+                        ingredientValueDictionary[ingredientType] = ingredientValue.value;
+                    }
+                    else
+                    {
+                        Debug.LogWarning($"Unknown IngredientType: {ingredientValue.type}");
+                    }
+                }
             }
         }
+    }
+
+    private static string ExtractJsonString()
+    {
+        return File.ReadAllText(IngredientValueJsonPath);
     }
 }
